5fcd89c105ccde9a7c005c2b18ffc1b8c5f7b944
rycr3278 <rycr3278@colorado.edu>
2023-10-24 20:45:53 -0400

    with comments

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import pygame
 import os
+
+# initialize pygame modules
 pygame.font.init()
 pygame.mixer.init()
 
+# constants
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First game!")
 
+# colors
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
+# game elements
 BORDER = pygame.Rect((WIDTH//2) - 3, 0, 6, HEIGHT)
 BACKPLATE_L = pygame.Rect(5, 10, 250, 45)
 BACKPLATE_R = pygame.Rect(645, 10, 250, 45)
 BACKPLATE_WINNER = pygame.Rect(50, 200, 770, 100)
 
+# sounds
 BULLET_HIT_SOUND = pygame.mixer.Sound('Assets/mixkit-electronic-retro-block-hit-2185.wav')
 BULLET_FIRE_SOUND = pygame.mixer.Sound('Assets/mixkit-explainer-video-game-alert-sweep-236.wav')
 WINNER_MUSIC = pygame.mixer.Sound('Assets/mixkit-arcade-mechanical-bling-210.wav')
 MUSIC = pygame.mixer.Sound('Assets/digital-love-127441.mp3')
 
-
+#fonts
 HEALTH_FONT = pygame.font.SysFont('couriernew', 40)
 WINNER_FONT = pygame.font.SysFont('couriernew', 100)
 
+# game settings
 FPS = 60
 VEL = 5
 BULLET_VEL = 7
 MAX_BULLETS = 3
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
+# custom events
 player1_HIT = pygame.USEREVENT + 1
 player2_HIT = pygame.USEREVENT + 2
 
+# spaceship images
 player1_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'ship1.png'))
 player1_SPACESHIP = pygame.transform.rotate(
     pygame.transform.scale(
@@ -45,9 +54,10 @@ player2_SPACESHIP = pygame.transform.rotate(
     pygame.transform.scale(
         player2_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
+# background image
 SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'background2.png')), (WIDTH, HEIGHT))
 
-
+# draw game window
 def draw_window(player2, player1, player2_bullets, player1_bullets, player2_health, player1_health):
     WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
@@ -70,6 +80,7 @@ def draw_window(player2, player1, player2_bullets, player1_bullets, player2_heal
     
     pygame.display.update()
 
+# player1 movement
 def player1_handle_movement(keys_pressed, player1):
     if keys_pressed[pygame.K_a] and player1.x - VEL > 0: # left
         player1.x -= VEL
@@ -79,7 +90,8 @@ def player1_handle_movement(keys_pressed, player1):
         player1.y -= VEL
     if keys_pressed[pygame.K_s] and player1.y + VEL  + player1.height < HEIGHT - 20: # down
         player1.y += VEL
-            
+
+# player2 movement            
 def player2_handle_movement(keys_pressed, player2):
     if keys_pressed[pygame.K_LEFT] and player2.x - VEL > BORDER.x + 20: # left
         player2.x -= VEL
@@ -90,6 +102,7 @@ def player2_handle_movement(keys_pressed, player2):
     if keys_pressed[pygame.K_DOWN] and player2.y + VEL  + player2.height < HEIGHT - 20: # down
         player2.y += VEL
 
+# bullet movement and collisions
 def handle_bullets(player1_bullets, player2_bullets, player1, player2):
     for bullet in player1_bullets:
         bullet.x += BULLET_VEL
@@ -107,7 +120,7 @@ def handle_bullets(player1_bullets, player2_bullets, player1, player2):
         elif bullet.x < 0:
             player2_bullets.remove(bullet)
        
-       
+# display winner       
 def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
     pygame.draw.rect(WIN, BLACK, BACKPLATE_WINNER)    
@@ -115,7 +128,8 @@ def draw_winner(text):
                          2, HEIGHT/2 - draw_text.get_height()/2))
     pygame.display.update()
     pygame.time.delay(5000)
-    
+
+# main game loop    
 def main():
     #MUSIC.play()
     player2 = pygame.Rect(800, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -178,6 +192,6 @@ def main():
 
 main()    
     
-    
+# entry point    
 if __name__ == "__main__":
     main()
